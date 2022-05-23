bda91524b711d8d61a7aac9be2065ed4970d67e8
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-23 16:44:48 +0100

    패키지 및 모듈 호출 개념 정리 완료

--- travel/thailand.py
+++ travel/thailand.py
@@ -1,3 +1,12 @@
 class ThailandPackage:
     def detail(self):
         print("태국 패키지 3박 4일, 방콕 파타야 여행 및 야시장 투어")
+
+if __name__ == "__main__":
+    print("Thailand 모듈을 직접 실행한다.")
+    print("이 문장은 모듈을 직접 실행 시 실행됩니다.")
+    trip_to = ThailandPackage()
+    trip_to.detail()
+else:
+    print("Thailand 외부에서 모듈호출이 이루어졌습니다.")
+    # 파일 외부에서 호출 시 여기가 호출된다.
