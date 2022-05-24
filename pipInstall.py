5e5be00a08b29de49e6d9f7cbedb9a1865fc5a8b
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-24 17:05:59 +0100

    개념 정리 완료

--- pipInstall.py
+++
@@ -1,9 +0,0 @@
-# 파이선에는 정말 다양한 라이브러리가 존재한다pypi에 가면 진짜 수도없이 많다.
-# 인스톨 할때는 pip3 install [library name]
-# 언인스톨 때는 pip3 uninstall[library name]
-
-# 사용할때 예시(인스톨 후)
-
-# from bs4 import BeautifulSoup
-# soup = BeautifulSoup()
-# print(soup.pretty())
