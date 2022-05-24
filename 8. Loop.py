645529845414b3367c95359392becd39b2c2de5e
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-24 16:04:03 +0100

    루프 및 슬라이싱 개념 정리

--- 8. Loop.py
+++ 8. Loop.py
@@ -1,16 +1,18 @@
+### 반복문 개념 ###
+
 print("대기번호 : 1")
 print("대기번호 : 2")
 print("대기번호 : 3")
 
 for wating_no in [0, 2, 2, 5, 4]:
     print("대기번호 : {0}".format(wating_no))
-
-#리스트 내 값들에 대해서 하나씩 돌아가면서 인덱스 순서에 따라서
+# 리스트를 범위로 설정해서 반복문을 돌릴 수 있다.
+# 리스트 내 값들에 대해서 하나씩 돌아가면서 인덱스 순서에 따라서
 # 파라미터로 넣어주고 순서대로 출력해준다.
 
 for wating_no1 in range(1, 5):
     print("대기번호 : {0}".format(wating_no1))
-#범위를 지정해서 반복문을 지정해줄 수 있다. format보간법을 통해서 데이터를 입력해줄 수 있다.
+# 범위를 지정해서 반복문을 지정해줄 수 있다. format보간법을 통해서 데이터를 입력해줄 수 있다.
 
 
 starbucks = ["아이언맨", "토르", "아이엠그루트"]
@@ -18,8 +20,8 @@ starbucks = ["아이언맨", "토르", "아이엠그루트"]
 for customer in starbucks:
     print("스타벅스에는 {0}의 커피가 준비되었습니다.".format(customer))
 
-
-#while반복문에 대해서 공부해보자.
+#while 반복문에 대해서 공부해보자.
+#while 반복문은 주어진 조건이 false가 될 때 반복을 탈출한다.
 
 customer = "토르"
 index = 5
@@ -45,7 +47,7 @@ while index >= 1:
 #     print("{0}, 커피가 준비되었습니다.".format(customer))
 #     person = input("이름이 무엇인가요?")
 
-#인풋을 통해서 값에 체크되면 while구문을 탈출하게 된다.
+# 인풋을 통해서 값에 체크되면 while구문을 탈출하게 된다.
 
 
 #continue 와 break에 대해서 알아본다.
@@ -55,10 +57,13 @@ nobook = [7]
 
 for student in range(1, 11):
     if student in absent:
-        continue #이 조건에 대해서는 반복루프를 한 번 건너뛴다.
+        continue 
+    #이 조건에 대해서는 반복루프를 한 번 건너뛴다.
+    #하지만 반복은 계속된다.
     elif student in nobook:
         print("오늘 수업 여기까지, {0}는 교무실로 와".format(student))
-        break # 반복문을 탈출해버린다.
+        break 
+    # 반복문을 탈출해버린다.
     print("{0}야 책을 읽어보거라".format(student))
 
 
@@ -69,6 +74,7 @@ print(student)
 student = [i+100 for i in student]
 print(student)
 #이렇게 해주면 101, 102, 103, 104, 105가 배열에 들어가있다.
+#파이선의 특징 같은데, 리스트 내에서 반복성질을 정의해서 재정의를 해줄 수 있다.
 
 students = ["Ironman", "Thor", "I am groot"]
 students_int = [len(i) for i in students]
