92d7dc759fc9022dc272b457459607f5aa9779f0
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-28 22:00:28 +0100

    비밀 경매 프로그램 코딩 완료

--- PythonUdemy/Source_Codes/Coding_Practice.py
+++ PythonUdemy/Source_Codes/Coding_Practice.py
@@ -1,19 +1,53 @@
 ## 소수 체크 프로그램 ##
 
-n = int(input("Please give me the number, I will check whether the number is prime or not."))
-print("number we got is " + str(n))
+# n = int(input("Please give me the number, I will check whether the number is prime or not."))
+# print("number we got is " + str(n))
+#
+# def prime_checker(number):
+#     a = 0
+#
+#     for i in range(1, number):
+#         if number % i == 0:
+#             a += 1
+#
+#     print(a)
+#
+#     if a >= 2:
+#         print("The number is not prime number")
+#     else:
+#         print("The number is prime number.")
+#
+# prime_checker(n)
 
-def prime_checker(number):
-    a = 0
+### 비밀 경매 프로그램 코딩 ###
 
-    for i in range(1, number):
-        if number % i == 0:
-            a += 1
+from replit import clear
+from time import *
+import math
 
-    print(a)
-    if a >= 2:
-        print("The number is not prime number")
-    else:
-        print("The number is prime number.")
+user_info = {}
 
-prime_checker(n)
+for i in range(3):
+    user_name = input("What is your name?")
+    user_price = int(input("What is your price?")) 
+    user_info[user_name] = user_price
+    clear()
+
+print(user_info)
+
+prices = list(user_info.values())
+users = list(user_info.keys())
+
+winner_price = max(prices)
+print(winner_price)
+## 배열에서 최대값 조회
+
+winner_price_index = prices.index(winner_price)
+print(winner_price_index)
+## index 메서드를 통해 최대값 인덱스를 조회
+
+winner_user_name = users[winner_price_index]
+print("Winner of bid is " + winner_user_name)
+
+## 경매 프로그램 코딩 완료
+
