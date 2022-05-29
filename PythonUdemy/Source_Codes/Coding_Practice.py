b1596e1487ae241ab797673c859fae7df1b8accd
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-29 11:17:52 +0100

    터틀 그래픽 코딩문제 처리 완료

--- PythonUdemy/Source_Codes/Coding_Practice.py
+++ PythonUdemy/Source_Codes/Coding_Practice.py
@@ -21,33 +21,67 @@
 
 ### 비밀 경매 프로그램 코딩 ###
 
-from replit import clear
-from time import *
-import math
+# from replit import clear
+# from time import *
+# import math
 
-user_info = {}
+# user_info = {}
 
-for i in range(3):
-    user_name = input("What is your name?")
-    user_price = int(input("What is your price?")) 
-    user_info[user_name] = user_price
-    clear()
+# for i in range(3):
+#     user_name = input("What is your name?")
+#     user_price = int(input("What is your price?")) 
+#     user_info[user_name] = user_price
+#     clear()
 
-print(user_info)
+# print(user_info)
 
-prices = list(user_info.values())
-users = list(user_info.keys())
+# prices = list(user_info.values())
+# users = list(user_info.keys())
 
-winner_price = max(prices)
-print(winner_price)
-## 배열에서 최대값 조회
+# winner_price = max(prices)
+# print(winner_price)
+# ## 배열에서 최대값 조회
 
-winner_price_index = prices.index(winner_price)
-print(winner_price_index)
-## index 메서드를 통해 최대값 인덱스를 조회
+# winner_price_index = prices.index(winner_price)
+# print(winner_price_index)
+# ## index 메서드를 통해 최대값 인덱스를 조회
 
-winner_user_name = users[winner_price_index]
-print("Winner of bid is " + winner_user_name)
+# winner_user_name = users[winner_price_index]
+# print("Winner of bid is " + winner_user_name)
 
 ## 경매 프로그램 코딩 완료
 
+from inspect import isgenerator
+from secrets import choice
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(500, 400)
+
+colors = ["red", "blue", "yellow", "black", "green", "purple"]
+y_position = [-70, -40, -10, 20, 50, 80]
+velosity = [10, 20, 30, 20, 10]
+turtles = []
+
+for index in range(0, 6):
+    new_turtle = Turtle("turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[index])
+    new_turtle.goto(x=-230, y=y_position[index])
+    turtles.append(new_turtle)
+
+print(turtles)
+
+is_game_end = False
+
+while not is_game_end:
+    for index in range(len(turtles)):
+        turtle = turtles[index]
+        turtle.forward(choice(velosity))
+
+        if turtle.pos()[0] == 200:
+            is_game_end = True
+            print("The game end, winner is " + turtle.pencolor())
+
+
+screen.exitonclick()
