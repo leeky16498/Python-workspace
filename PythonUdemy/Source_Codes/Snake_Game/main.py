8938464ab81eab7d8d7d918637c45343bd01012a
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-30 15:53:19 +0100

    스네이크 게임 최신화

--- PythonUdemy/Source_Codes/Snake_Game/main.py
+++ PythonUdemy/Source_Codes/Snake_Game/main.py
@@ -1,16 +1,21 @@
 from turtle import Screen, Turtle
+from scoreboard import Scoreboard
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
 screen.setup(width=600, height=400)
-screen.bgcolor("black")
+screen.bgcolor("blue")
 screen.title("Snake game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
-screen.listen()
+screen.listen() 
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
@@ -23,5 +28,10 @@ while game_is_on:
     time.sleep(0.1)
     ## 뒤의 블럭이 앞의 블럭을 밀어내며 이동하도록 코딩함.
     snake.move()
+    
+    ## 먹이와의 충돌여부를 결정한다.
+    ## 터틀의 distance 메서드를 통해서 좌표에 접근여부를 체크한다.
+    if snake.head.distance(food) < 15:
+        food.refresh()
         
 screen.exitonclick()
