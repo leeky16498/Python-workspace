6fce5001a465e52579111cfe7350eebf19f6b881
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-31 16:29:27 +0100

    파일매니저 공부중

--- PythonUdemy/Source_Codes/Snake_Game/scoreboard.py
+++ PythonUdemy/Source_Codes/Snake_Game/scoreboard.py
@@ -6,6 +6,7 @@ class Scoreboard(Turtle):
         super().__init__()
         self.score = 0
         self.color("white")
+        self.high_score = 0
         self.penup()
         self.goto(0, 170)
         self.write(f"Score : {self.score}", align="center", font=("Arial", 24, "normal"))
@@ -22,3 +23,7 @@ class Scoreboard(Turtle):
     def game_over(self):
         self.goto(0,0)
         self.write("Game_Over", align="center", font=("Arial", 24, "normal"))
+    
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
