ba8e5d114a4515ee86390e4b42ac7d6763f4cf4f
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-05-31 14:07:46 +0100

    퐁 게임 공 바운스 구현

---
+++ PythonUdemy/Source_Codes/pong_game/ball.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 20
+    
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+    
+    def bounce_y(self):
+        self.y_move *= -1 #벽에 부딫히면 방향을 틀어준다.
+    
+    def bounce_x(self):
+        self.x_move *= -1
--- PythonUdemy/Source_Codes/pong_game/main.py
+++ PythonUdemy/Source_Codes/pong_game/main.py
@@ -1,6 +1,7 @@
 from tkinter import Pack
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
@@ -9,6 +10,8 @@ screen.setup(800, 800)
 screen.title("Pong game")
 screen.tracer(0)
 ## 애니메이션의 통제여부를 결정하는 메서드이다.
+
+ball = Ball()
 
 paddle1 = Paddle()
 paddle1.penup()
@@ -25,6 +28,13 @@ screen.onkey(paddle1.controller_down, "Down")
 game_is_on  = True
 
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
-
-screen.exitonclick()
+    ball.move()
+    
+    if ball.ycor() > 380 or ball.ycor() < -380:
+        ball.bounce_y()
+        
+    # 패들과의 접촉을 체크해준다.
+    if ball.distance(paddle1) < 50 and ball.xcor() > 340 or ball.distance(paddle2) < 50 and ball.xcor() < -380:
+        ball.bounce_x()
