7e136e2c0c1a9cf4efce2a4730ebc6bfb067bcba
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-03 20:40:09 +0100

    에러 핸들링 시작

--- python_udemy/bootcamp_Angela/Source_Codes/ETC_CODES/error_handling.py
+++ python_udemy/bootcamp_Angela/Source_Codes/ETC_CODES/error_handling.py
@@ -5,3 +5,29 @@
 # TypeError : 타입이 맞지 않을 때, 
 
 ##위의 에러들을 처리하기 위해서 우리는 에러 핸들링을 실시한다.
+
+'''
+1. try : something that might cause an exception
+2. except : Do this if there was an exception
+3. else: Do this if there were no exception
+4. finally : Do this no matter what happens
+
+'''
+
+try:
+    file = open("a_file.txt")
+except:
+    file = open("a_file.txt", "w")
+    file.write("something")
+    
+try:
+    file = open("a_file.txt")  
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("something")
+except KeyError:
+    print("fcu!")
+## 다음과 같이 구체적인 에러의 발생에 대해서도 가지치기가 가능하다.
+    
+    
+## 조건문처럼 에러를 핸들링해서 프로그램의 강제 종료를 막는다.
