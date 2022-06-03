3364586bae0ade54419f3cf1074539dac5b5114a
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-03 20:42:46 +0100

    에러 처리 개념

--- python_udemy/bootcamp_Angela/Source_Codes/ETC_CODES/error_handling.py
+++ python_udemy/bootcamp_Angela/Source_Codes/ETC_CODES/error_handling.py
@@ -25,9 +25,16 @@ try:
 except FileNotFoundError:
     file = open("a_file.txt", "w")
     file.write("something")
-except KeyError:
-    print("fcu!")
+except KeyError as error:
+    print("fcu!" + error)
 ## 다음과 같이 구체적인 에러의 발생에 대해서도 가지치기가 가능하다.
+# as 구문으로 error를 연관값으로 체크해서 전달이 가능하다.
+else:
+    print("hahah")
+    ##여기는try에서 에러체크가 안될 때 씰행된다.
+finally:
+    file.close()
+    ##여기는 무조건 이 마지막에 상관없이 실행된다.
     
     
 ## 조건문처럼 에러를 핸들링해서 프로그램의 강제 종료를 막는다.
