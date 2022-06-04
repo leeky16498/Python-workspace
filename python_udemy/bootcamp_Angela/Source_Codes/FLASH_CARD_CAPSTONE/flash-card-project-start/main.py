843efd3b7380fb1fb54fe4339c906b243ab1e5d5
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-04 16:53:30 +0100

    플래시카드 작성 마무리 완료

--- python_udemy/bootcamp_Angela/Source_Codes/FLASH_CARD_CAPSTONE/flash-card-project-start/main.py
+++ python_udemy/bootcamp_Angela/Source_Codes/FLASH_CARD_CAPSTONE/flash-card-project-start/main.py
@@ -4,8 +4,13 @@ from turtle import right
 import time
 import random
 #--------------- Get data from CSV ---------------#
-data = pd.read_csv("/Users/kyungyunlee/Desktop/PYTHON/PYTHON_UDEMY/BOOTCAMP_ANGELA/SOURCE_CODES/FLASH_CARD_CAPSTONE/flash-card-project-start/data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+
+try:  
+    data = pd.read_csv("/Users/kyungyunlee/Desktop/PYTHON/PYTHON_UDEMY/BOOTCAMP_ANGELA/SOURCE_CODES/FLASH_CARD_CAPSTONE/flash-card-project-start/data/word_to_learn.csv")
+except FileNotFoundError:
+    data = pd.read_csv("/Users/kyungyunlee/Desktop/PYTHON/PYTHON_UDEMY/BOOTCAMP_ANGELA/SOURCE_CODES/FLASH_CARD_CAPSTONE/flash-card-project-start/data/french_words.csv")    
+finally:
+    to_learn = data.to_dict(orient="records")
 current_card = {}
 
 print(to_learn)
@@ -24,6 +29,15 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=back_image)
+
+def is_known():
+    to_learn.remove(current_card) ## 지정된 딕셔너리 자체를 직접 지우는게 가능하다.
+    
+    data = pd.DataFrame(to_learn)
+    data.to_csv("/Users/kyungyunlee/Desktop/PYTHON/PYTHON_UDEMY/BOOTCAMP_ANGELA/SOURCE_CODES/FLASH_CARD_CAPSTONE/flash-card-project-start/data/word_to_learn.csv")
+    
+    
+    next_card()
 
     
 ## French : English 딕셔너리 작성 완료.
@@ -49,7 +63,7 @@ button1 = Button(image=right_image, highlightthickness=0, borderwidth=0, command
 button1.grid(column=1, row=1)
 
 wrong_image = PhotoImage(file="/Users/kyungyunlee/Desktop/PYTHON/PYTHON_UDEMY/BOOTCAMP_ANGELA/SOURCE_CODES/FLASH_CARD_CAPSTONE/flash-card-project-start/images/wrong.png")
-button2 = Button(image=wrong_image, highlightthickness=0, borderwidth=0, command=next_card)
+button2 = Button(image=wrong_image, highlightthickness=0, borderwidth=0, command=is_known)
 button2.grid(column=3, row=1)
 
 next_card()
