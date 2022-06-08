7d4694ea267abf55f5037a024b7c723eba67cd5d
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-08 13:44:01 +0100

    커밋

--- python_udemy/HAGMANN_AL_TRAIDING/Part1_Materials/Blackboard/trading_bot.py
+++ python_udemy/HAGMANN_AL_TRAIDING/Part1_Materials/Blackboard/trading_bot.py
@@ -1,11 +1,6 @@
-from cgitb import text
-from binance.client import Client
-import websocket
-import pandas as pd
-import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np 
 
-my_api = "eSpIC1boIZDc8yaLDIJZOMMCMUnhsPxXNefUjuq6r7PRnubuQcO3e4koe9KLYY0x"
-my_secret = "mH7WXrNg2ldpFoysSundyFCZErIM9ovTDj4nfWP09GnLF9sXh68GtsqC4z7yQGLK"
-
-client = Client(api_key=my_api, api_secret=my_secret, tld="com", testnet=True)
-client.get_account()
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plot.show()
