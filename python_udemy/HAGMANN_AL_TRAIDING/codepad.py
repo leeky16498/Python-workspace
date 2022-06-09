82ab16e46aad0e17e3bdbd5cbf5a0c5588458622
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-09 19:09:14 +0100

    commit

---
+++ python_udemy/HAGMANN_AL_TRAIDING/codepad.py
@@ -0,0 +1,8 @@
+from binance.client import Client
+import websocket, json
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
+
+API_KEY = "OkNTS53iVnQwQ4SvVn9rScwpqGbuX37uzAboebgssjVC53ZPBBOIycYE7tWcgKoA"
+SECRET_KEY = "lIStreQbr4ob7I6x5A7DpOJHIRmb5shNDIve79H56A7kAfhcDoe1qLapUnHF4wt1"
