7c0aa24680b275293de930fc7461a30f6de1f414
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-13 16:05:04 +0100

    챕터 8

--- OPENCV_TUTORIAL/CHAPTER_8.py
+++ OPENCV_TUTORIAL/CHAPTER_8.py
@@ -15,6 +15,23 @@ def get_contours(img):
             print(len(approx))
             obj_cor = len(approx)
             x, y, w, h = cv2.boundingRect(approx)
+            
+            if obj_cor == 3:
+                objectType = "Tri"
+            elif obj_cor == 4:
+                asp_ratio = w/float(h)
+                if asp_ratio > 0.95 and asp_ratio <  1.05:
+                    objectType = "None"
+                else:
+                    objectType = "Rectangle"
+            elif obj_cor > 4:
+                objectType = "Circle"
+            else:
+                objectType = "None"
+            
+            cv2.rectangle(img_contour, (x, y), (x+w, y+w), (0, 255, 0))
+            cv2.putText(img_contour, objectType, (x+(w//2)-10, y+(h//2)-10), 
+                        cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 0), 2)
     
 path = "/Users/kyungyunlee/Desktop/PYTHON/OPENCV_TUTORIAL/dog.png"
 img = cv2.imread(path)
@@ -24,7 +41,7 @@ img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 img_blur = cv2.GaussianBlur(img_gray, (7, 7), 1)
 img_canny = cv2.Canny(img_blur, 50, 50)
 
-cv2.inshow("original", img)
+cv2.imshow("original", img)
 cv2.imshow("gray", img_gray)
 cv2.imshow("blur", img_blur)
 cv2.imshow("canny", img_canny)
