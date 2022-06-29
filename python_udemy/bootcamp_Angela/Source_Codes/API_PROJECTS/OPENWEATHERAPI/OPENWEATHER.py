05982c6a8c2ca6c54aa7ae419fe4d21524c0c60b
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-29 14:08:09 +0100

    opencv

---
+++ python_udemy/bootcamp_Angela/Source_Codes/API_PROJECTS/OPENWEATHERAPI/OPENWEATHER.py
@@ -0,0 +1,36 @@
+import requests
+from twilio.rest import Client
+
+OWM_ENDPOINT = "https://api.openweathermap.org/data/3.0/onecall"
+API_KEY = "fa0cff1c4cd7c8a9dd28bbef2155cfff"
+
+## twilio
+ACCOUNT_SID = "AC7d825129dbb493b9a6847a413997b048"
+AUTH_TOKEN = "38274575c6c7a10c4125021498068642"
+
+weather_params = {
+    "lat" : 51.507351,
+    "lon" : -0.127758,
+    "appid" : API_KEY, # 마지막에도 쉼표 찍어줘야 한다.
+    "exclude" : "current,minutely,daily",
+}
+
+response = requests.get(OWM_ENDPOINT, params=weather_params)
+weather_data = response.json()
+
+weather_slice = weather_data["hourly"][:12] # 리스트 슬라이싱을 시도한다.
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+        
+if will_rain:
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages.create(
+        body="It's gonna be raining, please check your umbrella!",
+        to="+447413435831"
+        )
+    
+print(message.status)
