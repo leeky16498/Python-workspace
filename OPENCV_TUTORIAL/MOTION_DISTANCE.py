9c104daca98f59cdc473e035db5df01026d41937
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-29 14:10:16 +0100

    빈 커밋

--- OPENCV_TUTORIAL/MOTION_DISTANCE.py
+++ OPENCV_TUTORIAL/MOTION_DISTANCE.py
@@ -17,14 +17,14 @@ while True:
 	thresh = cv2.dilate(thresh, None, 3)
 	thresh = cv2.erode(thresh, np.ones((4,4)), 1)
 	contor,_ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cv2.circle(prev, (40,200), 5, (0,0,255), -1)
+	cv2.circle(prev, (200,200), 5, (0,0,255), -1)
 	for contors in contor:				
 		if cv2.contourArea(contors) > 30000:
 			(x,y,w,h) = cv2.boundingRect(contors)
 			(x1,y1),rad = cv2.minEnclosingCircle(contors)
 			x1 = int(x1)
 			y1 = int(y1)
-			cv2.line(prev, (20,200), (x1, y1), (255,0,0), 4)
+			cv2.line(prev, (200,200), (x1, y1), (255,0,0), 4)
 			cv2.putText(prev, "{}".format(int(np.sqrt((x1 - 20)**2 + (y1 - 200)**2))), (100,100),cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
 			cv2.rectangle(prev, (x,y), (x+w,y+h), (0,255,0), 2)
 			cv2.circle(prev, (x1,y1), 5, (0,0,255), -1)
