7764abad24c92af28304a4c720a3a3b06124a799
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-06-30 20:17:20 +0100

    opencv클래스

--- OPENCV_TUTORIAL/MOTION_DISTANCE.py
+++ OPENCV_TUTORIAL/MOTION_DISTANCE.py
@@ -10,24 +10,24 @@ new = cv2.flip(new, 1)
 
 
 while True:
-	diff = cv2.absdiff(prev, new)
-	diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+	diff = cv2.absdiff(prev, new) # 움직임이 있다면 diff가 0이 아닐 것이며, 움직임이 감지되면 diff가 0이다.
+	diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)# 그리고 그 움직임의 차이 픽셀을 회색계열로 변환해주고
 	diff = cv2.blur(diff, (5,5))
-	_,thresh = cv2.threshold(diff, 10, 255, cv2.THRESH_BINARY)
+	_,thresh = cv2.threshold(diff, 10, 255, cv2.THRESH_BINARY) # 그리고 threshold함수를 통해서 thresh 바이너리로 변경해준다.
 	thresh = cv2.dilate(thresh, None, 3)
 	thresh = cv2.erode(thresh, np.ones((4,4)), 1)
 	contor,_ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cv2.circle(prev, (200,200), 5, (0,0,255), -1)
+	cv2.circle(prev, (200,200), 5, (0,0,255), 10)
 	for contors in contor:				
 		if cv2.contourArea(contors) > 30000:
-			(x,y,w,h) = cv2.boundingRect(contors)
-			(x1,y1),rad = cv2.minEnclosingCircle(contors)
-			x1 = int(x1)
+			(x,y,w,h) = cv2.boundingRect(contors) # 바운드를 찾고 그 요소를 체크한다.
+			(x1,y1),rad = cv2.minEnclosingCircle(contors) # 중심좌표를 찾는다.
+			x1 = int(x1)# 각 값을 정수로 변환해주고
 			y1 = int(y1)
-			cv2.line(prev, (200,200), (x1, y1), (255,0,0), 4)
-			cv2.putText(prev, "{}".format(int(np.sqrt((x1 - 20)**2 + (y1 - 200)**2))), (100,100),cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
-			cv2.rectangle(prev, (x,y), (x+w,y+h), (0,255,0), 2)
-			cv2.circle(prev, (x1,y1), 5, (0,0,255), -1)
+			cv2.line(prev, (200,200), (x, y), (255,0,0), 4) # 선을 긋는다.
+			cv2.putText(prev, "length : {}".format(int(np.sqrt((x1 - 20)**2 + (y1 - 200)**2))), (100,100),cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
+			cv2.rectangle(prev, (x,y), (x+w,y+h), (0,255,0), 2) # 사각형을 그려서 움직임이 감지된 영역을 그려준다.
+			cv2.circle(prev, (x,y), 5, (0,0,255), 10)
 		
 					
 	cv2.imshow("orig", prev)
@@ -36,7 +36,7 @@ while True:
 	_, new = cap.read()
 	new = cv2.flip(new, 1)
 
-	if cv2.waitKey(1) == 27:
+	if cv2.waitKey(1) == 27: ## esc키를 누르면 프로그램을 종료한다.
 		break
 
 cap.release()
--- OPENCV_TUTORIAL/camera.py
+++ OPENCV_TUTORIAL/camera.py
@@ -6,4 +6,13 @@ video_capture = cv2.VideoCapture(0) # 넘버 0는 웹켐을 의미, 1은 다른 
 while True:
     ret, frame = video_capture.read()
     image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    detections = face_detector.detectMul
+    detections = face_detector.detectMultiScale(image_gray, minSize = (100, 100))
+    
+    for (x, y, w, h) in detections:
+        print(w, h)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    cv2.imshow('video', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
