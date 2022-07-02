07f5c59d862dcd84bd187d46b2856afef086e946
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-07-02 15:52:31 +0100

    stocktracker

---
+++ python_udemy/bootcamp_Angela/Source_Codes/API_PROJECTS/STOCKTRACKER/main.py
@@ -0,0 +1,75 @@
+import requests
+from datetime import datetime, timedelta
+
+now = datetime.now()
+now = now.date()
+yesterday = str(now - timedelta(days = 1))
+before_yesterday = str(now - timedelta(days = 2))
+print(yesterday)
+print(before_yesterday)
+
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+STOCK_API_KEY = "1XFJ9WQW3KRQOUFU"
+NEWS_API_KEY = "f3ca7ffbf6294c20bd64db7ba5ff8d04"
+
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+
+stock_params = {
+    "function" : "TIME_SERIES_DAILY",
+    "symbol" : STOCK,
+    "apikey" : STOCK_API_KEY
+}
+
+url = "https://www.alphavantage.co/query?"
+r = requests.get(url, params=stock_params)
+r.raise_for_status()
+data = r.json()
+
+yesterday_price = float(data['Time Series (Daily)'][str(yesterday)]["4. close"])
+before_yesterday_price = float(data['Time Series (Daily)'][str(before_yesterday)]["4. close"])
+percentage = (before_yesterday_price - yesterday_price) / before_yesterday_price * 100
+
+## STEP 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+def finding_article(company):
+    global now
+    url = "https://newsapi.org/v2/everything?"
+    news_params = {
+        "q" : company,
+        "from" : now,
+        "apikey" : NEWS_API_KEY
+    }
+    r = requests.get(url, news_params)
+    r.raise_for_status()
+    data = r.json()
+    data = data["articles"][:3]
+    
+    for datum in data:
+        print("title : {}".format(datum["title"]))
+        print("description : {}".format(datum["description"]))
+        print("-" * 100)
+
+if percentage > 5 or percentage < -5:
+    print("Start to check the articles for Tesla!")
+else:
+    finding_article(COMPANY_NAME)
+
+
+## STEP 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+
+
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
