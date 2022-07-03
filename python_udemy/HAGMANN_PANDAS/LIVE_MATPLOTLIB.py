60e8443fee6a8be2349fbb97fc93e076cd263a7d
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-07-03 12:08:09 +0100

    맷플롯립 데이터 반영중

---
+++ python_udemy/HAGMANN_PANDAS/LIVE_MATPLOTLIB.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+from matplotlib import style  
+
+style.use('fivethirtyeight')
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 1, 1)
+
+def animate(i):
+    graph_data = open()
