7e8becfbd9aaa68a794beed3521dff380e315376
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-07-03 12:52:42 +0100

    simple_generator

---
+++ python_udemy/HAGMANN_PANDAS/LIVE_GRAPH/simple_animator.py
@@ -0,0 +1,23 @@
+
+import random
+from itertools import count
+from matplotlib.animation import FuncAnimation
+import pandas as pd
+import matplotlib.pyplot as plt
+
+plt.style.use('fivethirtyeight')
+
+x_vals = []
+y_vals = []
+
+index = count()
+
+def animate(i):
+    x_vals.append(next(index))
+    y_vals.append(random.randint(0, 5))
+    
+    plt.plot(x_vals, y_vals)
+    
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+plt.tight_layout()
+plt.show()
