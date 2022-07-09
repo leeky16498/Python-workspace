e3773c97b8445427ea18a148cff4118a58f00940
devleeky16498 <92150863+leeky16498@users.noreply.github.com>
2022-07-09 21:03:27 +0100

    개인 프로젝트

--- SELF_PROJECT/naver_economic.py
+++ SELF_PROJECT/naver_economic.py
@@ -1,13 +1,20 @@
-import requests
-import pywhatkit as pwk
 
+import requests
+import smtplib
+from email.mime.text import MIMEText
+from datetime import datetime
+
+now = datetime.now().strftime("%m/%d/%y")
 CLIENT_ID = "geleXkjPByfkP9liYJUB"
 CLIENT_SECRET = "Zow_vh_uXo"
 API_ENDPOINT = "https://openapi.naver.com/v1/search/news.json"
+MY_EMAIL = "leeky16498@gmail.com"
+MY_PASSWORD = "tfcawoypiogozmwh"
+MAIL_SUBJECT = "하루 시작하며 봐야할 경제 및 기업뉴스 20개 (from. 남편, {}])".format(now)
 
 API_PARAMS = {
-    'query' : '경제 top',
-    'display' : 10,
+    'query' : '경제 기업',
+    'display' : 20,
     'start' : 1,
     'sort' : "sim"
 }
@@ -20,13 +27,24 @@ API_headers = {
 r = requests.get(url=API_ENDPOINT, params=API_PARAMS, headers=API_headers)
 r.raise_for_status()
 data = r.json()
-print(data["items"])
 count = 1
-str = ""
-
+str = "오늘의 경제 및 기업관련 20개 네이버 뉴스\n\n"
 
 for value in data["items"]:
     str += '{}. title : {}\nlink : {}\n\n'.format(count, value["title"], value["link"])
     count += 1
-    
-pwk.sendwhatmsg("+447413435831", str, 7, 26)
+
+print(str)
+
+#-----email sending----#
+
+smtp = smtplib.SMTP('smtp.gmail.com', 587)
+smtp.ehlo()
+smtp.starttls()
+smtp.login(MY_EMAIL, MY_PASSWORD)
+
+msg = MIMEText(str)
+msg['Subject'] = MAIL_SUBJECT
+
+smtp.sendmail(MY_EMAIL, ["leeky16498@gmail.com", "suhyeon106@gmail.com"], msg.as_string())
+smtp.close()
--- note.py
+++ note.py
@@ -1,4 +1,33 @@
-str = "대한민국"
-# str = list(str)
-print(str)
-print(str[2])
+import pickle
+
+courses_rooms = {'CS101':3004, 'CS102':4501, 'CS103':6755, 'NT110':1224,
+                 'CM241':1411}
+courses_instructors = {'CS101':'Haynes','CS102':'Alvarado','CS103':'Rich',
+                      'NT110':'Burke','CM241':'Lee'}
+courses_times = {'CS101':'8:00 a.m.','CS102':'9:00 a.m.','CS103':'10:00 a.m.',
+                'NT110':'11:00 a.m.','CM241':'1:00 p.m.'}
+
+
+file = open('classpickle.pickle','wb')
+pickle.dump(courses_rooms, file)
+pickle.dump(courses_instructors, file)
+pickle.dump(courses_times, file)
+file.close()
+
+course = True
+
+while course == True:
+    course = input('\nEnter a course number or press "q" to quit: ')
+    print(course)
+    
+    if course in courses_rooms.keys():
+        print()
+        print('Room: ',courses_rooms[course])
+        print('Instructor: ',courses_instructors[course])
+        print('Time: ',courses_times[course])
+        
+    elif course == 'q' or 'Q':
+        break
+    
+    else:
+        print("That is not appropriate characters")
