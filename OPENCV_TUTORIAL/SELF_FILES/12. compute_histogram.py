f6459899e9d9e6ef9357f2ca4c8310e093378ba5
Kyungyun_Lee <leeky16498@gmail.com>
2022-07-25 14:00:17 +0100

    파일 추가

--- OPENCV_TUTORIAL/SELF_FILES/12. compute_histogram.py
+++ OPENCV_TUTORIAL/SELF_FILES/12. compute_histogram.py
@@ -11,20 +11,34 @@ cv2.imshow('gray', gray)
 
 blank = np.zeros(img.shape[:2], dtype='uint8')
 mask = cv2.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
-mask = cv2.bitwise_and(gray, gray, mask=mask)
-cv2.imshow('mask', mask)
+mask_img = cv2.bitwise_and(gray, gray, mask=mask)
+cv2.imshow('mask_img', mask_img)
 
 #g_scale hist
 # gray_hist = cv2.calcHist([gray], [0], None, [256], [0, 256])
 gray_hist = cv2.calcHist([gray], [0], mask,[256], [0, 256])
 
+# plt.figure()
+# plt.title("gray_hist")
+# plt.xlabel("Bins")
+# plt.ylabel("num of pixels")
+# plt.plot(gray_hist)
+# plt.show()
+# 기본적으로 밝은 이미지, 하얀색에 가까운 이미지가 높은 값을 가지게 된다.
+
+# colours Histogram
 plt.figure()
 plt.title("gray_hist")
 plt.xlabel("Bins")
 plt.ylabel("num of pixels")
-plt.plot(gray_hist)
-plt.show()
-# 기본적으로 밝은 이미지, 하얀색에 가까운 이미지가 높은 값을 가지게 된다.
+    
+colors = ('b', 'g', 'r')
 
+for i, col in enumerate(colors):
+    hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+    plt.plot(hist, color=col)
+    plt.xlim([0, 256])
+
+plt.show()
 
 cv2.waitKey(0)
